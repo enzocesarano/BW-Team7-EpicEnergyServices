1eb69e4b4e8ef53e32666c5dca429cd380a0eb80
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-11-11 12:09:28 +0100

    Initial commit

---
+++ src/main/java/team7/EpicEnergyServices/EpicEnergyServicesApplication.java
@@ -0,0 +1,13 @@
+package team7.EpicEnergyServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyServicesApplication.class, args);
+	}
+
+}
